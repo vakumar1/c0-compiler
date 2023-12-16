5321d99ff1cba7dd6249b8f855861f51e98b7fe0
Vasanth Kumar <vakumar@berkeley.edu>
2023-12-15 23:39:41 -0800

    [Elab, IR] Clean state mgmt; add uq scope id w/ var IR name generation (fixes shared var IR name across scopes bug)

--- src/Ir.hs
+++ src/Ir.hs
@@ -18,193 +18,224 @@ import qualified Data.Map as Map
 irFunction :: FunctionElab -> ([BasicBlockIr], [VerificationError])
 irFunction fn =
     let initBb = BasicBlockIr [] []
-        initScope = Scope [Map.empty] 0
-     in irSeq fn (functionElabBlock fn) (BasicBlockIr [] []) initScope []
+        emptyState = State [] 0 0
+        initState = addScope emptyState
+     in irSeq fn (functionElabBlock fn) initBb initState []
 
-irSeq :: FunctionElab -> SeqElab -> BasicBlockIr -> Scope -> [VerificationError] -> ([BasicBlockIr], [VerificationError])
-irSeq fn ss bb scope errs =
+irSeq :: FunctionElab -> SeqElab -> BasicBlockIr -> State -> [VerificationError] -> ([BasicBlockIr], [VerificationError])
+irSeq fn ss bb state errs =
     case ss of
         -- i. terminate current basic block
         [] -> ([bb], errs)
         RET_ELAB r : _ ->
-            let (retComms, _, retErrs) = irRet r fn scope
+            let (retComms, _, retErrs) = irRet r fn state
                 bbRet = BasicBlockIr (bbIrArgs bb) (retComms ++ (bbIrCommands bb))
              in ([bbRet], retErrs ++ errs)
         
         -- ii. extend current basic block
         DECL_ELAB d : _ ->
-            let (declComms, declScope, declErrs) = irDecl d scope
+            let (declComms, declState, declErrs) = irDecl d state
                 bbDecl = BasicBlockIr (bbIrArgs bb) (declComms ++ (bbIrCommands bb))
-             in irSeq fn (tail ss) bbDecl declScope (declErrs ++ errs)
+             in irSeq fn (tail ss) bbDecl declState (declErrs ++ errs)
         ASN_ELAB a : _ ->
-            let (asnComms, asnScope, asnErrs) = irAsn a scope
+            let (asnComms, asnState, asnErrs) = irAsn a state
                 bbAsn = BasicBlockIr (bbIrArgs bb) (asnComms ++ (bbIrCommands bb))
-             in irSeq fn (tail ss) bbAsn asnScope (asnErrs ++ errs)
+             in irSeq fn (tail ss) bbAsn asnState (asnErrs ++ errs)
 
         -- iii. start new basic block
         SEQ_ELAB s : _ ->
-            let currBbArgs = getIrArgs (scopeMaps scope)
+            let currBbArgs = getIrArgs (scopes state)
 
                 -- evaluate for inner block
                 innerBb = BasicBlockIr currBbArgs []
-                innerScope = Scope (Map.empty:(scopeMaps scope)) (scopeRegCtr scope)
-                (innerSeqBb, innerSeqErrs) = irSeq fn s innerBb innerScope []
+                innerState = addScope state
+                (innerSeqBb, innerSeqErrs) = irSeq fn s innerBb innerState []
 
                 -- evaluate for remainder of block
                 nextBb = BasicBlockIr currBbArgs []
-                (nextSeqBb, nextSeqErrs) = irSeq fn (tail ss) nextBb scope []
+                (nextSeqBb, nextSeqErrs) = irSeq fn (tail ss) nextBb state []
 
             -- concat results from existing bb, inner bb(s), and remaining bb(s)
             in (nextSeqBb ++ innerSeqBb ++ [bb], nextSeqErrs ++ innerSeqErrs ++ errs)
 
-irDecl :: DeclElab -> Scope -> ([CommandIr], Scope, [VerificationError])
-irDecl (DeclElab varElab Nothing) scope =
+irDecl :: DeclElab -> State -> ([CommandIr], State, [VerificationError])
+irDecl (DeclElab varElab Nothing) state =
     let name = extractIdentifierName (variableElabIdentifier varElab)
-        ty = typeElabType (variableElabType varElab)
-        varScopeMap = Map.insert name varElab (head (scopeMaps scope))
-        varScope = Scope (varScopeMap : (tail (scopeMaps scope))) (scopeRegCtr scope)
-     in case Map.lookup name (head (scopeMaps scope)) of
+        varState = insertToTopScope state name varElab
+     in case Map.lookup name (scopeMap (head (scopes state))) of
             Just prevVarElab ->
                 let err = (DOUBLE_DECL (DoubleDeclarationError (variableElabIdentifier prevVarElab) (variableElabIdentifier varElab)))
-                 in ([], varScope, [err])
+                 in ([], state, [err])
             Nothing ->
-                let varScopeMap = Map.insert name varElab (head (scopeMaps scope))
-                    varScope = Scope (varScopeMap : (tail (scopeMaps scope))) (scopeRegCtr scope)
-                    var = VariableIr name ty False
+                let varState = insertToTopScope state name varElab
+                    var = varElabToIr varElab (scopeId (head (scopes state)))
                     comm = INIT_IR var
-                 in ([comm], varScope, [])
-irDecl (DeclElab varElab (Just asn)) scope =
-    let (comm1, sco1, err1) = irDecl (DeclElab varElab Nothing) scope
-        (comm2, sco2, err2) = irAsn asn sco1
-     in (comm2 ++ comm1, sco2, err2 ++ err1)
+                 in ([comm], varState, [])
+irDecl (DeclElab varElab (Just asn)) state =
+    let (declComms, declState, declErrs) = irDecl (DeclElab varElab Nothing) state
+        (asnComms, asnState, asnErrs) = irAsn asn declState
+     in ((asnComms ++ declComms), asnState, (asnErrs ++ declErrs))
 
-irAsn :: AsnElab -> Scope -> ([CommandIr], Scope, [VerificationError])
-irAsn (AsnElab tok e) scope =
-    let (comm1, m_pt1, sco1, err1) = irExp e scope
-        m_var = identifierLookup tok (scopeMaps scope)
-     in case m_var of
+irAsn :: AsnElab -> State -> ([CommandIr], State, [VerificationError])
+irAsn (AsnElab tok e) state =
+    let (expComms, m_expPT, expState, expErrs) = irExp e state
+        m_varElab = identifierLookup tok (scopes state)
+     in case m_varElab of
             -- check that varElab is declared
-            Just varElab ->
-                case m_pt1 of
-                    Just (pu1, ty1) ->
+            Just (varElab, varScopeId) ->
+                case m_expPT of
+                    Just (expPu, expTy) ->
                         -- check that var and exp have the same type
                         let varTy = (typeElabType (variableElabType varElab))
-                         in if varTy == ty1
-                                then (comm1, sco1, err1)
-                                else ([], scope, (ASN_TYPE_MISMATCH (AsnTypeMismatch tok varTy ty1)) : err1)
+                            asnComm = ASN_PURE_IR (varElabToIr varElab varScopeId) expPu
+                         in if varTy == expTy
+                                then ((asnComm:expComms), expState, expErrs)
+                                else ([], state, (ASN_TYPE_MISMATCH (AsnTypeMismatch tok varTy expTy)) : expErrs)
                     Nothing ->
-                        ([], scope, err1)
+                        ([], state, expErrs)
             -- fail if varElab is not declared
             Nothing ->
-                ([], scope, (USE_BEFORE_DECL (UseBeforeDeclarationError tok)) : err1)
+                ([], state, (USE_BEFORE_DECL (UseBeforeDeclarationError tok)) : expErrs)
 
-irRet :: RetElab -> FunctionElab -> Scope -> ([CommandIr], Scope, [VerificationError])
-irRet (RetElab e) (FunctionElab _ (TypeElab retTy _) _) scope =
-    let (comm1, m_pt1, sco1, err1) = irExp e scope
-     in case m_pt1 of
-            Just (pu1, ty1) ->
-                if (retTy == ty1)
+irRet :: RetElab -> FunctionElab -> State -> ([CommandIr], State, [VerificationError])
+irRet (RetElab e) (FunctionElab _ (TypeElab retTy _) _) state =
+    let (expComms, m_expPT, expState, expErrs) = irExp e state
+     in case m_expPT of
+            Just (expPu, expTy) ->
+                if (retTy == expTy)
                     then
-                        let comm2 = RET_PURE_IR pu1
-                         in (comm2 : comm1, sco1, err1)
-                    else ([], scope, (RET_TYPE_MISMATCH (RetTypeMismatch retTy ty1)) : err1)
+                        let retComm = RET_PURE_IR expPu
+                         in (retComm : expComms, expState, expErrs)
+                    else ([], state, (RET_TYPE_MISMATCH (RetTypeMismatch retTy expTy)) : expErrs)
             Nothing ->
-                ([], scope, err1)
+                ([], state, expErrs)
 
-irExp :: ExpElab -> Scope -> ([CommandIr], Maybe (PureIr, TypeCategory), Scope, [VerificationError])
-irExp e scope =
+irExp :: ExpElab -> State -> ([CommandIr], Maybe (PureIr, TypeCategory), State, [VerificationError])
+irExp e state =
     case e of
-        CONST_ELAB c -> irConst c scope
-        IDENTIFIER_ELAB i -> irIdentifier i scope
-        BINOP_ELAB b -> irBinop b scope
-        UNOP_ELAB u -> irUnop u scope
+        CONST_ELAB c -> irConst c state
+        IDENTIFIER_ELAB i -> irIdentifier i state
+        BINOP_ELAB b -> irBinop b state
+        UNOP_ELAB u -> irUnop u state
 
-irConst :: Const -> Scope -> ([CommandIr], Maybe (PureIr, TypeCategory), Scope, [VerificationError])
-irConst const scope = ([], Just (CONST_IR const, constToType const), scope, [])
+irConst :: Const -> State -> ([CommandIr], Maybe (PureIr, TypeCategory), State, [VerificationError])
+irConst const state = ([], Just (CONST_IR const, constToType const), state, [])
 
-irIdentifier :: Token -> Scope -> ([CommandIr], Maybe (PureIr, TypeCategory), Scope, [VerificationError])
-irIdentifier tok scope =
-    case identifierLookup tok (scopeMaps scope) of
-        Just varElab ->
-            let varName = (extractIdentifierName (variableElabIdentifier varElab))
+irIdentifier :: Token -> State -> ([CommandIr], Maybe (PureIr, TypeCategory), State, [VerificationError])
+irIdentifier tok state =
+    case identifierLookup tok (scopes state) of
+        Just (varElab, varScopeId) ->
+            let varIr = varElabToIr varElab varScopeId
                 varTypeCat = (typeElabType (variableElabType varElab))
-                varIr = VariableIr varName varTypeCat False
-             in ([], Just (VAR_IR varIr, varTypeCat), scope, [])
+             in ([], Just (VAR_IR varIr, varTypeCat), state, [])
         Nothing ->
-            ([], Nothing, scope, [(USE_BEFORE_DECL (UseBeforeDeclarationError tok))])
+            ([], Nothing, state, [(USE_BEFORE_DECL (UseBeforeDeclarationError tok))])
 
-irBinop :: BinopElab -> Scope -> ([CommandIr], Maybe (PureIr, TypeCategory), Scope, [VerificationError])
-irBinop (BinopElab cat op e1 e2) scope =
-    let (comm1, m_pt1, sco1, err1) = irExp e1 scope
-        (comm2, m_pt2, sco2, err2) = irExp e2 sco1
-     in case (m_pt1, m_pt2) of
+irBinop :: BinopElab -> State -> ([CommandIr], Maybe (PureIr, TypeCategory), State, [VerificationError])
+irBinop (BinopElab cat op e1 e2) state =
+    let (expComms1, m_expPT1, expState1, expErrs1) = irExp e1 state
+        (expComms2, m_expPT2, expState2, expErrs2) = irExp e2 expState1
+     in case (m_expPT1, m_expPT2) of
             -- type check when we have 2 valid exps
-            (Just pt1, Just pt2) ->
-                let (pu1, ty1) = pt1
-                    (pu2, ty2) = pt2
-                    m_infType = binopTypeInf cat ty1 ty2
+            (Just (expPu1, expTy1), Just (expPu2, expTy2)) ->
+                let m_infType = binopTypeInf cat expTy1 expTy2
                  in case m_infType of
                         -- return pure exp with inferred type
-                        Just ty3 ->
-                            let (comm3, pu3, sco3) = binopOpTranslate cat ty3 pu1 pu2 sco2
-                             in (comm3 ++ comm2 ++ comm1, Just (pu3, ty3), sco3, err2 ++ err1)
+                        Just infType ->
+                            let (binopComms, binopPu, binopState) = binopOpTranslate cat infType expPu1 expPu2 expState2
+                             in (binopComms ++ expComms2 ++ expComms1, Just (binopPu, infType), binopState, expErrs2 ++ expErrs1)
                         -- fail when type for binop cannot be inferred
                         Nothing ->
-                            let err = OP_TYPE_MISMATCH (OpTypeMismatch op [ty1, ty2])
-                             in ([], Nothing, scope, err : (err2 ++ err1))
+                            ([], Nothing, state, (OP_TYPE_MISMATCH (OpTypeMismatch op [expTy1, expTy2])) : (expErrs2 ++ expErrs1))
             -- immediately fail if either exp is invalid
             _ ->
-                ([], Nothing, scope, err2 ++ err1)
+                ([], Nothing, state, expErrs2 ++ expErrs1)
 
-irUnop :: UnopElab -> Scope -> ([CommandIr], Maybe (PureIr, TypeCategory), Scope, [VerificationError])
-irUnop (UnopElab cat op e) scope =
-    let (comm1, m_pt1, sco1, err1) = irExp e scope
-     in case m_pt1 of
+irUnop :: UnopElab -> State -> ([CommandIr], Maybe (PureIr, TypeCategory), State, [VerificationError])
+irUnop (UnopElab cat op e) state =
+    let (expComms, m_expPT, expState, expErrs) = irExp e state
+     in case m_expPT of
             -- type check when the exp is valid
-            Just pt1 ->
-                let (pu1, ty1) = pt1
-                    m_infType = unopTypeInf cat ty1
+            Just (expPu, expTy) ->
+                let m_infType = unopTypeInf cat expTy
                  in case m_infType of
                         -- return pure exp with inferred type
-                        Just ty2 ->
-                            let (comm2, pu2) = unopOpTranslate cat pu1
-                             in (comm2 ++ comm1, Just (pu2, ty2), sco1, err1)
+                        Just infType ->
+                            let (unopComms, unopPu) = unopOpTranslate cat expPu
+                             in (unopComms ++ expComms, Just (unopPu, infType), expState, expErrs)
                         -- fail when type for unop cannot be inferred
                         Nothing ->
-                            let err = OP_TYPE_MISMATCH (OpTypeMismatch op [ty1])
-                             in ([], Nothing, scope, err : err1)
+                            ([], Nothing, state, (OP_TYPE_MISMATCH (OpTypeMismatch op [expTy])) : expErrs)
             -- immediately fail if exp is invalid
             _ ->
-                ([], Nothing, scope, err1)
+                ([], Nothing, state, expErrs)
 
--- HELPERS
+-- MANAGE AND ACCESS ELABORATED STATE
 
-data Scope = Scope
-    { scopeMaps :: [Map.Map String VariableElab]
-    , scopeRegCtr :: Int
+data State = State
+    { scopes :: [Scope]
+    , regCtr :: Int
+    , mapCtr :: Int
     }
 
-getIrArgs :: [Map.Map String VariableElab] -> [VariableIr]
-getIrArgs maps = 
-    let singleMap = foldr getIrArgsFoldFn Map.empty maps
+data Scope = Scope
+    { scopeMap :: Map.Map String VariableElab
+    , scopeId :: Int
+    }
+
+addTemp :: State -> (String, State)
+addTemp state = 
+    let tempName = (show (regCtr state)) ++ ":temp"
+    in (tempName, State (scopes state) ((regCtr state) + 1) (mapCtr state))
+
+addScope :: State -> State
+addScope state = 
+    let newScope = Scope Map.empty (mapCtr state)
+    in State (newScope:(scopes state)) (regCtr state) ((mapCtr state) + 1)
+
+insertToTopScope :: State -> String -> VariableElab -> State
+insertToTopScope state name varElab = 
+    let topScope = head (scopes state)
+        varScope = Scope (Map.insert name varElab (scopeMap topScope)) (scopeId topScope)
+    in State (varScope : (tail (scopes state))) (regCtr state) (mapCtr state)
+
+popScopeMap :: State -> State
+popScopeMap state = State (tail (scopes state)) (regCtr state) (mapCtr state)
+
+identifierLookup :: Token -> [Scope] -> Maybe (VariableElab, Int)
+identifierLookup tok scopes =
+    case scopes of
+        [] -> Nothing
+        scope : _ ->
+            case (Map.lookup (extractIdentifierName tok) (scopeMap scope)) of
+                Just var -> Just (var, (scopeId scope))
+                Nothing -> identifierLookup tok (tail scopes)
+
+varElabToIr :: VariableElab -> Int -> VariableIr
+varElabToIr varElab varScopeId = 
+    VariableIr ((show varScopeId) ++ ":" ++ (extractIdentifierName (variableElabIdentifier varElab)))
+                (typeElabType (variableElabType varElab)) 
+                False
+
+--  BB ARGUMENT EXTRACTION
+
+-- merges all scopes (from right to left to prioritize higher scopes)
+-- maps all varElabs (with corresponding scope id) to varIr
+getIrArgs :: [Scope] -> [VariableIr]
+getIrArgs scopes = 
+    let singleMap = foldr getIrArgsFoldFn Map.empty scopes
     in map getIrArgsTranslateFn (Map.toList singleMap)
 
-getIrArgsFoldFn :: Map.Map String VariableElab -> Map.Map String VariableElab -> Map.Map String VariableElab
-getIrArgsFoldFn initMap nextMap = 
-    foldr (\(s, v) interMap -> Map.insert s v interMap) initMap (Map.toList nextMap)
+-- iterates through all elements in SCOPE and inserts (with scope id) into INIT_MAP
+getIrArgsFoldFn :: Scope -> Map.Map String (Int, VariableElab) -> Map.Map String (Int, VariableElab)
+getIrArgsFoldFn scope initMap = 
+    let currScopeId = (scopeId scope)
+    in foldr (\(s, v) interMap -> Map.insert s (currScopeId, v) interMap) initMap (Map.toList (scopeMap scope))
 
-getIrArgsTranslateFn :: (String, VariableElab) -> VariableIr
-getIrArgsTranslateFn (varName, varElab) = 
-    VariableIr varName (typeElabType (variableElabType varElab)) False
+getIrArgsTranslateFn :: (String, (Int, VariableElab)) -> VariableIr
+getIrArgsTranslateFn (_, (varScopeId, varElab)) = varElabToIr varElab varScopeId
 
-identifierLookup :: Token -> [Map.Map String VariableElab] -> Maybe VariableElab
-identifierLookup tok maps =
-    case maps of
-        [] -> Nothing
-        _ ->
-            case (Map.lookup (extractIdentifierName tok) (head maps)) of
-                Just var -> Just var
-                Nothing -> identifierLookup tok (tail maps)
+-- OP HELPERS
 
 binopTypeInf :: BinopCatElab -> TypeCategory -> TypeCategory -> Maybe TypeCategory
 binopTypeInf cat t1 t2 =
@@ -212,22 +243,24 @@ binopTypeInf cat t1 t2 =
         (INT_TYPE, INT_TYPE) -> Just INT_TYPE
         _ -> Nothing
 
-binopOpTranslate :: BinopCatElab -> TypeCategory -> PureIr -> PureIr -> Scope -> ([CommandIr], PureIr, Scope)
-binopOpTranslate cat ty p1 p2 scope =
+binopOpTranslate :: BinopCatElab -> TypeCategory -> PureIr -> PureIr -> State -> ([CommandIr], PureIr, State)
+binopOpTranslate cat ty p1 p2 state =
     case cat of
-        ADD_EXP_ELAB -> ([], PURE_BINOP_IR (ADD_IR p1 p2), scope)
-        SUB_EXP_ELAB -> ([], PURE_BINOP_IR (SUB_IR p1 p2), scope)
-        MUL_EXP_ELAB -> ([], PURE_BINOP_IR (MUL_IR p1 p2), scope)
+        ADD_EXP_ELAB -> ([], PURE_BINOP_IR (ADD_IR p1 p2), state)
+        SUB_EXP_ELAB -> ([], PURE_BINOP_IR (SUB_IR p1 p2), state)
+        MUL_EXP_ELAB -> ([], PURE_BINOP_IR (MUL_IR p1 p2), state)
         DIV_EXP_ELAB ->
-            let temp = VariableIr (show (scopeRegCtr scope)) ty True
+            let (tempName, newState) = addTemp state
+                temp = VariableIr tempName ty True
                 binop = DIV_IR p1 p2
                 comm = ASN_IMPURE_BINOP_IR temp binop
-             in ([comm], VAR_IR temp, Scope (scopeMaps scope) ((scopeRegCtr scope) + 1))
+             in ([comm], VAR_IR temp, newState)
         MOD_EXP_ELAB ->
-            let temp = VariableIr (show (scopeRegCtr scope)) ty True
+            let (tempName, newState) = addTemp state
+                temp = VariableIr tempName ty True
                 binop = MOD_IR p1 p2
                 comm = ASN_IMPURE_BINOP_IR temp binop
-             in ([comm], VAR_IR temp, Scope (scopeMaps scope) ((scopeRegCtr scope) + 1))
+             in ([comm], VAR_IR temp, newState)
 
 unopTypeInf :: UnopCatElab -> TypeCategory -> Maybe TypeCategory
 unopTypeInf cat t1 =
